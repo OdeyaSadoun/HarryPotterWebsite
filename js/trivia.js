20e5c60cbd30959f8ff06b0bbb13389bf38646b8
אודיה סעדון <osadoun@g.jct.ac.il>
2023-10-29 21:04:49 +0200

    trivia- not fix

--- js/trivia.js
+++ js/trivia.js
@@ -1,24 +1,32 @@
-
 let currentQuestion = 1;
-    const totalQuestions = 5;
-    let score = 0;
+const totalQuestions = 5;
+let score = 0;
 
-    function showQuestion(questionNumber) {
-      document.getElementById(`question${currentQuestion}`).style.display = 'none';
-      currentQuestion = questionNumber;
-      document.getElementById(`question${currentQuestion}`).style.display = 'block';
+function resetQuiz(){
+    score = 0;
+    document.getElementById(`question${totalQuestions}`).style.display = "none";
+    document.getElementById(`question1`).style.display = "block";
+}
+
+function showQuestion(questionNumber) {
+  document.getElementById(`question${currentQuestion}`).style.display = "none";
+  currentQuestion = questionNumber;
+  document.getElementById(`question${currentQuestion}`).style.display = "block";
+}
+
+function calculateScore() {
+  for (let i = 1; i <= totalQuestions; i++) {
+    const selectedAnswer = document.querySelector(
+      `input[name="q${i}"]:checked`
+    );
+    if (selectedAnswer && selectedAnswer.value === 'correct') {
+      score += 20;
     }
+  }
 
-    function calculateScore() {
-      for (let i = 1; i <= totalQuestions; i++) {
-        const selectedAnswer = document.querySelector(`input[name="q${i}"]:checked`);
-        if (selectedAnswer && selectedAnswer.value === "correct") {
-          score += 20;
-        }
-      }
+  const results = document.getElementById('results');
+  results.innerHTML = `<h3>Your Score: ${score} out of 100</h3>`;
+  document.getElementById('resetButton').hidden = false;
+}
 
-      const results = document.getElementById("results");
-      results.innerHTML = `<h3>Your Score: ${score} out of 100</h3>`;
-    }
-
-    showQuestion(1);
+showQuestion(1);
